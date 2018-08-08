cb572d6cf7b6969410e98dfae9cf247edb2376fe
claire-1 <claire8991@gmail.com>
2018-08-07 20:50:38 -0700

    Updated to show gifs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // exporting app in nodejs
-module.exports = (app) => {
+/*module.exports = (app) => {
   // app.log logs info
   app.log('Yay! The app was loaded!')
 
@@ -16,5 +16,25 @@ module.exports = (app) => {
     // Post a comment on the issue
     // docs: https://octokit.github.io/rest.js/#api-Gists-createComment
     return context.github.issues.createComment(params)
+  })
+}*/
+
+const got = require('got') // required in package.json b/c imported got $ nmp install got
+module.exports = (app) => {
+  app.on('issues.opened', async context => {
+    if (context.payload.issue.body.includes(`/GIFME`)) {
+      // process.env.GIPHY_API_KEY
+      const searchTerm = context.payload.issue.body.split(`/GIFME`).join(' ')
+      // URL based on giphy api; needs to have ` (backticks) to work
+      const url = `http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${searchTerm}`
+      let giphy = await got(url)
+      giphy = JSON.parse(giphy.body)
+      // app.log(giphy) // for debugging
+      // data[0] to get first image only
+      const gifURL = giphy.data[0].images.original.url
+      
+      const params = context.issue({body: `![cats gif](${gifURL})`})
+      return context.github.issues.createComment(params)
+      }
   })
 }
